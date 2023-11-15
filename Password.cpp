edc6866bf23bcaf5427e979c234ccfec77c1f80e
Shadowlief <shadowfire510@gmail.com>
2023-11-15 12:12:11 -0800

    updated shit

--- Password.cpp
+++ Password.cpp
@@ -3,6 +3,14 @@
 
 using std::string;
 
+//password constructor!
+Password::Password()
+{
+  string passcode = "ChicoCA-95929";
+  vector<string> pass_history;
+  pass_history.push_back(passcode);
+};
+
 /*
   The function receives a string counts how many times the same character 
   occurs at the beginning of the string, before any other characters (or the
@@ -25,6 +33,7 @@ int Password::count_leading_characters(string phrase){
    The function recieves a string, checks to see if there's an upper case letter and a lower case letter 
 */
 
+/*my version-fuck me it seg faults
 bool Password::has_mixed_case(string phrase){
   bool mixed = false;
   bool hasLowerCase = false;
@@ -32,11 +41,13 @@ bool Password::has_mixed_case(string phrase){
   int currASCII = 0;
   for(int i = 0; i < phrase.length()-1; i++)
   {
-    if() //ASCII val is from 65-90 [an upper case letter]
+    currASCII = int(phrase[i]); //typecast the character at this point of the string into an int to get the ASCII code.
+
+    if(currASCII >= 65 && currASCII <=90) //ASCII val is from 65-90 [an upper case letter]
     {
       hasUpperCase = true;
     }
-    if() //ASCII val is from #-# [a lower case letter]
+    if(currASCII >= 97 && currASCII <= 122) //ASCII val is from 97-122 [a lower case letter]
     {
       hasLowerCase = true;
     }
@@ -46,4 +57,85 @@ bool Password::has_mixed_case(string phrase){
     mixed = true;
   }
   return mixed;
+}*/
+
+
+//tony version
+bool Password::has_mixed_case(string phrase)
+{
+  if(phrase.size() == 0){ //if input in empty
+    return false;
+  }
+  bool upper = false;
+  bool lower = false;
+  for(int i = 0; i < phrase.length(); i++){
+    if(isupper(phrase[i])) //check if current charactrer is uppercase
+      upper = true;
+    if(islower(phrase[i])) //check if current character is lowercase 
+      lower = true;
+  }
+  return upper && lower; //return true if both are true
 }
+
+//Starting here everything breaks and I don't know where the hiding {} is!!
+
+/*
+  receives a password and sets the latest in pass_history to it if and only
+  if it meets all criteria:
+    1. The password is at least 8 letters long
+    2. It has no more than 3 of the same leading characters
+    3. It has mixed case (at least one upper case and at least one lower case)
+    4. It was not a previous password in the history
+  */
+  void Password::set(string currentCode)
+  {
+    if(authenticate(currentCode))
+    {
+      pass_history.push_back(currentCode);
+    }
+  }
+
+
+  /*
+  receives a string and authenticates it against the latest password in the
+  pass_history, returning true for an exact match or false when it does not match
+  or if a password has not been set.
+  */
+  bool Password::authenticate(string testME)
+  {
+    bool isGood = true;
+
+    //passcode length test
+    if(testME.length() < 8)
+    {
+      isGood = false;
+      return isGood;
+    }
+
+    //leading character check
+    int rep = count_leading_characters(testME);
+    if(rep > 3)
+    {
+      isGood = false;
+      return isGood;
+    }
+    
+    //mixed case check
+    if(!has_mixed_case(testME))
+    {
+      isGood = false;
+      return isGood;
+    }
+
+    //previous passcode test
+    for(int i = 0; i < pass_history.size(); i++)
+    {
+      if(testME == pass_history.at(i))
+      {
+        isGood = false;
+        return isGood;
+      }
+    }
+
+    return isGood;
+  }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -47,6 +47,89 @@ TEST(PasswordTest, empty_string) //does it get mad if there's nothing in the str
 TEST(PasswordTest, all_one_letter) //does it get mad if we have something like aAaA?
 {
 	Password word;
-	ASSERT_TRUE();
+	ASSERT_TRUE(word.has_mixed_case("aAaAaA"));
 }
-Test(PasswordTest, ) //is it happy about
+TEST(PasswordTest, space_between_stuff) //is it happy about spaces within the password?
+{
+	Password word;
+	ASSERT_TRUE(word.has_mixed_case("a     b    C   d"));
+}
+
+TEST(PasswordTest, only_weird_characters) //does it evaluate weird characters as false?
+{
+	Password word;
+	ASSERT_FALSE(word.has_mixed_case("!@#*^_)+/<."));
+}
+
+TEST(PasswordTest, leet_Speek_True) //Is it happy with nums mixed in with chars?
+{
+	Password word;
+	ASSERT_TRUE(word.has_mixed_case("4ppleP13"));
+}
+TEST(PasswordTest, leet_Speek_False)
+{
+	Password word;
+	ASSERT_FALSE(word.has_mixed_case("4ppl3p13"));
+}
+
+TEST(PasswordTest, only_spaces) //Is it not happy with the password with the contents being only spaces?
+{
+	Password word;
+	ASSERT_FALSE(word.has_mixed_case("            "));
+} 
+TEST(PasswordTest, newLine_broke_in_BUT_its_true) //if a new line somehow snuck into a correct passcode but it's still true, is it true?
+{
+	Password word;
+	ASSERT_TRUE(word.has_mixed_case("dum '\n' IE")); //double check with prof about this line
+}
+TEST(PasswordTest, newLine_broke_in_AND_its_FALSE) //new line is in, and it's false
+{
+	Password word;
+	ASSERT_FALSE(word.has_mixed_case("dum '\n' ie"));
+}
+TEST(PasswordTest, long_ass_password) //is it able to handle THE ENTIRE FUCKING KEYBOARD being used as a password?
+{
+	Password word;
+	ASSERT_TRUE(word.has_mixed_case("QqWwEeRrTtYyUuIiOoPp{[}]|\\AaSsDdFfGgHhJjKkLl:;\"'ZzXxCcVvBbNnMm<,>./?`~!1@2#3$4%5^6&7*8(9)0_-+="));
+}
+
+
+/*TEST(PasswordTest, set_a_new_passcode_true) //can set put in a correct new passcode?
+{
+	Password word;
+	//there has to be a better way to test if a string is correct!
+	word.set("Ch40$_D3m0n");
+	ASSERT_EQ("Ch40$_D3mon", );
+}
+
+TEST(PasswordTest, set_a_new_passcode_false) //can set realize that a passcode is false and therefore not set it?
+{
+	Password word;
+	word.set("F00kM3S3r10usly");
+	//fec how do I test this?!
+}
+*/
+
+TEST(PasswordTest, psscode_less_than_eight) //can authenticate find a password whose length is less than eight?
+{
+	Password word;
+	ASSERT_FALSE(word.authenticate("Dummy"));
+}
+
+TEST(PasswordTest, psscode_rep_bigger_than_three) //can authenticate find a passcode whose length is > 8 but also has 3 of the leading character
+{
+	Password word;
+	ASSERT_FALSE(word.authenticate("CraCClingCreatures"));
+}
+
+TEST(PasswordTest, psscode_no_mixed_case) //can authenticate find a passcode whose length is > 8, has <3 of the leading character, BUT doesn't have mixed case?
+{
+	Password word;
+	ASSERT_FALSE(word.authenticate("crackling"));
+}
+
+TEST(PasswordTest, psscode_original_passcode) //can authenticate find a passcode who is currently in the passcode history?
+{
+	Password word;
+	ASSERT_FALSE(word.authenticate("ChicoCA-95929"));
+}
